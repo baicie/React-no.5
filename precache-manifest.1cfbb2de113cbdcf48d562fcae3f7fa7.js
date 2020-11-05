21ae3a6a9b7280eaa7ac069632241cacde3e59d5
baicie <2055572879@qq.com>
2020-11-06 07:55:44 +0800

    20201106

---
+++ precache-manifest.1cfbb2de113cbdcf48d562fcae3f7fa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9558944f4468aa8be567af4dd23d6983",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c94bbb7e9fed370eebbe",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "0d38a3163b75cfeae707",
+    "url": "/static/css/main.baf8c220.chunk.css"
+  },
+  {
+    "revision": "c94bbb7e9fed370eebbe",
+    "url": "/static/js/2.c7f2c2e9.chunk.js"
+  },
+  {
+    "revision": "932507d561d9cea5ea27440adcb27ef2",
+    "url": "/static/js/2.c7f2c2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d38a3163b75cfeae707",
+    "url": "/static/js/main.4623ff9c.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
